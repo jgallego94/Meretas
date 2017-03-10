49f893d7f29c82526a6ceea176a897871ba9194a
Alex Doublet <Alex@Alexanders-MacBook-Air.local>
2017-03-10 16:58:53 -0700

    Back Code StoredProcedures
    Added code for Surveys storedprocedures and methods to call them in
    MeretasCodeHandler

--- App_Code/MeretasCodeHandler.cs
+++ App_Code/MeretasCodeHandler.cs
@@ -2,11 +2,67 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
 
-/// <summary>
-/// Summary description for CodeHandler
-/// </summary>
 public class MeretasCodeHandler
 {
-   
+   public bool AddQuestion(int QuestionID, int SurveyID, string Description)
+   {
+       bool Confirmation = false;
+       Surveys SurveyManager = new Surveys();
+
+       Confirmation = SurveyManager.AddQuestion(QuestionID, SurveyID, Description);
+
+       return Confirmation;
+   }
+
+   public Survey GetSurvey()
+   {
+       Surveys SurveyManager = new Surveys();
+       //Assuming you need a surveyId to return a survey
+       return SurveyManager.GetSurvey(SurveyID);
+   }
+
+
+   public bool AddResponse(int ResponseID, int QuestionID, int SurveyID, string Description)
+   {
+       bool Confirmation = false;
+       Surveys SurveyManager = new Surveys();
+
+       Confirmation = SurveyManager.AddResponse(ResponseID, QuestionID, SurveyId, Description)
+       
+       return Confirmation;
+   }
+
+   public bool AddSurvey(int SurveyID, string Description)
+   {
+       bool Confirmation = false;
+       Surveys SurveyManager = new Surveys();
+
+       Confirmation = SurveyManager.AddSurvey(SurveyId, Description)
+
+       return Confirmation;
+   }
+
+   public bool AddVisitorSurvey(int SurveyID, int VisitorID)
+   {
+       bool Confirmation = false;
+       Surveys SurveyManager = new Surveys();
+
+       Confirmation = SurveyManager.AddVisitorSurvey(SurveyID, VisitorID)
+
+       return Confirmation;
+   }
+
+   public bool ProcessSurvey(int VisitorID, int SurveyID, int QuestionID, int ResponseID, DateTime DateSubmitted, DateTime TimeSubmitted, int ResponseInstanceID)
+   {
+       bool Confirmation;
+       Surveys SurveyManager = new Surveys();
+
+       Confirmation = SurveyManager.ProcessSurvey(VisitorID, SurveyID, QuestionID, ResponseID, DateSubmitted, TimeSubmitted, ResponseInstanceID)
+
+       return Confirmation;
+   }
 }
--- App_Code/Surveys.cs
+++ App_Code/Surveys.cs
@@ -6,13 +6,10 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Configuration;
 
-/// <summary>
-/// Summary description for Surveys
-/// </summary>
-
 public class Surveys
 {
-    public Survey GetSurvey()
+    //using surveyID to return survey, still needs work
+    public Survey GetSurvey(int SurveyID)
     {
         Survey survey = new Survey(); 
 
@@ -38,5 +35,258 @@ public class Surveys
         }
 
         return survey;
+    }
+
+    public bool AddQuestion(int QuestionID, int SurveyId, string Description)
+    {
+        bool Successs = false;
+
+        ConnectionStringSettings WebSettings = ConfigurationManager.ConnectionStrings["Meretas"];
+        SqlConnection meretas = new SqlConnection();
+
+        meretas.ConnectionString = WebSettings.ConnectionString;
+        meretas.Open();
+
+        SqlCommand AddCommand = new SqlCommand();
+        AddCommand.Connection = meretas;
+        AddCommand.CommandType = CommandType.StoredProcedure;
+        AddCommand.CommandText = "AddQuestion";
+
+        SqlParameter QuestionIDParameter = new SqlParameter();
+        QuestionIDParameter.ParameterName = "@QuestionID";
+        QuestionIDParameter.SqlDbType = SqlDbType.int;
+        QuestionIDParameter.Value = QuestionID;
+        QuestionIDParameter.Direction = ParameterDirection.Input;
+
+        SqlParameter SurveyIDParameter = new SqlParameter();
+        SurveyIDParameter.ParameterName = "@SurveryID";
+        SurveyIDParameter.SqlDbType = SqlDbType.int;
+        SurveyIDParameter.Value = SurveryID;
+        SurveryIDParameter.Direction = SurveryIDParameter.Input;
+
+        SqlParameter DescriptionParameter = new SqlParameter();
+        DescriptionParameter.ParameterName = "@Description";
+        DescriptionParameter.SqlDbType = SqlDbType.VarChar;
+        DescriptionParameter.Value = Description;
+        DescriptionParameter.Direction = Input;
+
+        AddCommand.Parameters.Add(QuestionIDParameter);
+        AddCommand.Parameters.Add(SurveyIDParameter);
+        AddCommand.Parameters.Add(DescriptionParameter);
+
+        AddCommand.ExecuteNonQuery();
+
+        Success = true;
+
+        meretas.Close();
+
+        return Success; 
+    }
+
+    public bool AddResponse(int ResponseID, int QuestionID, int SurveyID, string Description)
+    {
+        bool Successs = false;
+
+        ConnectionStringSettings WebSettings = ConfigurationManager.ConnectionStrings["Meretas"];
+        SqlConnection meretas = new SqlConnection();
+
+        meretas.ConnectionString = WebSettings.ConnectionString;
+        meretas.Open();
+
+        SqlCommand AddCommand = new SqlCommand();
+        AddCommand.Connection = meretas;
+        AddCommand.CommandType = CommandType.StoredProcedure;
+        AddCommand.CommandText = "AddResponse";
+
+        SqlParameter ResponseIDParameter = new SqlParameter();
+        ResponseIDParameter.ParameterName = "@ResponseID";
+        ResponseIDParameter.SqlDbType = SqlDbType.int;
+        ResponseIDParameter.Value = ResponseID;
+        ResponseIDParameter.Direction = ParameterDirection.Input;
+
+        SqlParameter QuestionIDParameter = new SqlParameter();
+        QuestionIDParameter.ParameterName = "@QuestionID";
+        QuestionIDParameter.SqlDbType = SqlDbType.int;
+        QuestionIDParameter.Value = QuestionID;
+        QuestionIDParameter.Direction = ParameterDirection.Input;
+
+        SqlParameter SurveyIDParameter = new SqlParameter();
+        SurveyIDParameter.ParameterName = "@SurveryID";
+        SurveyIDParameter.SqlDbType = SqlDbType.int;
+        SurveyIDParameter.Value = SurveryID;
+        SurveryIDParameter.Direction = SurveryIDParameter.Input;
+
+        SqlParameter DescriptionParameter = new SqlParameter();
+        DescriptionParameter.ParameterName = "@Description";
+        DescriptionParameter.SqlDbType = SqlDbType.VarChar;
+        DescriptionParameter.Value = Description;
+        DescriptionParameter.Direction = Input;
+
+        AddCommand.Parameters.Add(ResponseIDParameter);
+        AddCommand.Parameters.Add(QuestionIDParameter);
+        AddCommand.Parameters.Add(SurveryIDParameter);
+        AddCommand.Parameters.Add(DescriptionParameter);
+
+        AddCommand.ExecuteNonQuery();
+
+        Successs = true;
+
+        meretas.Close();
+
+        return Success;
+
+    }
+
+    public bool AddSurvery(int SurveyID, string Description)
+    {
+        bool Success = false;
+
+        ConnectionStringSettings WebSettings = ConfigurationManager.ConnectionStrings["Meretas"];
+        SqlConnection meretas = new SqlConnection();
+
+        meretas.ConnectionString = WebSettings.ConnectionString;
+        meretas.Open();
+
+        SqlCommand AddCommand = new SqlCommand();
+        AddCommand.Connection = meretas;
+        AddCommand.CommandType = CommandType.StoredProcedure;
+        AddCommand.CommandText = "AddSurvey";
+
+        SqlParameter SurveyIDParameter = new SqlParameter();
+        SurveyIDParameter.ParameterName = "@SurveryID";
+        SurveyIDParameter.SqlDbType = SqlDbType.int;
+        SurveyIDParameter.Value = SurveryID;
+        SurveryIDParameter.Direction = SurveryIDParameter.Input;
+
+        SqlParameter DescriptionParameter = new SqlParameter();
+        DescriptionParameter.ParameterName = "@Description";
+        DescriptionParameter.SqlDbType = SqlDbType.VarChar;
+        DescriptionParameter.Value = Description;
+        DescriptionParameter.Direction = Input;
+
+        AddCommand.Parameters.Add(SurveyIDParameter);
+        AddCommand.Parameters.Add(DescriptionParameter);
+
+        AddCommand.ExecuteNonQuery();
+
+        Success = true;
+
+        meretas.Close();
+
+        return Success;
+    }
+
+    public bool AddVisitorSurvey(int SurveyID, int VisitorID)
+    {
+        bool Success = false;
+
+        ConnectionStringSettings WebSettings = ConfigurationManager.ConnectionStrings["Meretas"];
+        SqlConnection meretas = new SqlConnection();
+
+        meretas.ConnectionString = WebSettings.ConnectionString;
+        meretas.Open();
+
+        SqlCommand AddCommand = new SqlCommand();
+        AddCommand.Connection = meretas;
+        AddCommand.CommandType = CommandType.StoredProcedure;
+        AddCommand.CommandText = "AddVisitorSurvey";
+
+        SqlParameter SurveyIDParameter = new SqlParameter();
+        SurveyIDParameter.ParameterName = "@SurveryID";
+        SurveyIDParameter.SqlDbType = SqlDbType.int;
+        SurveyIDParameter.Value = SurveryID;
+        SurveryIDParameter.Direction = SurveryIDParameter.Input;
+
+        SqlParameter VisitorIDParameter = new SqlParameter();
+        VisitorIDParameter.ParameterName = "@VisitorID";
+        VisitorIDParameter.SqlDbType = SqlDbType.int;
+        VisitorIDParameter.Value = VisitorID;
+        VisitorIDParameter.Direction = ParameterDirection.Input;
+
+        AddCommand.Parameters.Add(SurveyIDParameter);
+        AddCommand.Parameters.Add(VisitorIDParameter);
+
+        AddCommand.ExecuteNonQuery();
+
+        Success = true;
+
+        meretas.Close();
+
+        return Success;
+        
+    }
+
+    //not sure if the dates will work, google said use DateTime, so I put that!
+    public bool ProcessSurvey(int VisitorID, int SurveyID, int QuestionID, int ResponseID, DateTime DateSubmitted, DateTime TimeSubmitted, int ResponseInstanceID)
+    {
+        bool Success = false;
+
+        ConnectionStringSettings WebSettings = ConfigurationManager.ConnectionStrings["Meretas"];
+        SqlConnection meretas = new SqlConnection();
+
+        meretas.ConnectionString = WebSettings.ConnectionString;
+        meretas.Open();
+
+        SqlCommand ProcessCommand = new SqlCommand();
+        ProcessCommand.Connection = meretas;
+        ProcessCommand.CommandType = CommandType.StoredProcedure;
+        ProcessCommand.CommandText = "ProcessSurvey";
+
+        SqlParameter VisitorIDParameter = new SqlParameter();
+        VisitorIDParameter.ParameterName = "@VisitorID";
+        VisitorIDParameter.SqlDbType = SqlDbType.int;
+        VisitorIDParameter.Value = VisitorID;
+        VisitorIDParameter.Direction = ParameterDirection.Input;
+
+        SqlParameter SurveyIDParameter = new SqlParameter();
+        SurveyIDParameter.ParameterName = "@SurveryID";
+        SurveyIDParameter.SqlDbType = SqlDbType.int;
+        SurveyIDParameter.Value = SurveryID;
+        SurveryIDParameter.Direction = SurveryIDParameter.Input;
+
+        SqlParameter QuestionIDParameter = new SqlParameter();
+        QuestionIDParameter.ParameterName = "@QuestionID";
+        QuestionIDParameter.SqlDbType = SqlDbType.int;
+        QuestionIDParameter.Value = QuestionID;
+        QuestionIDParameter.Direction = ParameterDirection.Input;
+
+        SqlParameter ResponseIDParameter = new SqlParameter();
+        ResponseIDParameter.ParameterName = "@ResponseID";
+        ResponseIDParameter.SqlDbType = SqlDbType.int;
+        ResponseIDParameter.Value = ResponseID;
+        ResponseIDParameter.Direction = ParameterDirection.Input;
+
+        SqlParameter DateSubmittedParameter = new SqlParameter();
+        DateSubmittedParameter.ParameterName = "@DateSubmitted";
+        DateSubmittedParameter.SqlDbType = SqlDbType.DateTime;
+        DateSubmittedParameter.Value = DateSubmitted;
+        DateSubmittedParameter.Direction = ParameterDirection.Input;
+
+        SqlParameter TimeSubmittedParameter = new SqlParameter();
+        TimeSubmittedParameter.ParameterName = "@TimeSubmitted";
+        TimeSubmittedParameter.SqlDbType = SqlDbType.DateTime;
+        TimeSubmittedParameter.Value = TimeSubmitted;
+        TimeSubmittedParameter.Direction = ParameterDirection.Input;
+
+        SqlParameter ResponseInstanceIDParameter = new SqlParameter();
+        ResponseInstanceIDParameter.ParameterName = "@ResponseInstanceID";
+        ResponseInstanceIDParameter.SqlDbType = SqlDbType.int;
+        ResponseInstanceIDParameter.Value = ResponseInstanceID;
+        ResponseInstanceIDParameter.Direction = ParameterDirection.Input;
+
+        ProcessCommand.Parameters.Add(VisitorIDParameter);
+        ProcessCommand.Parameters.Add(SurveyIDParameter);
+        ProcessCommand.Parameters.Add(ResponseIDParameter);
+        ProcessCommand.Parameters.Add(DateSubmittedParameter);
+        ProcessCommand.Parameters.Add(TimeSubmittedParameter);
+        ProcessCommand.Parameters.Add(ResponseInstanceIDParameter);
+
+        ProcessCommand.ExecuteNonQuery();
+
+        Success = true;
+
+        meretas.Close();
+
+        return Success;
     }
 }

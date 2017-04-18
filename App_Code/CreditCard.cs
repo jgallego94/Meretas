d821be6b56922bc3490d8307568bee7680f954f5
Jane Gallego <jgallego1@studentmail.nait.ca>
2017-04-18 14:22:29 -0600

    Added more properties to CreditCard

--- App_Code/CreditCard.cs
+++ App_Code/CreditCard.cs
@@ -10,51 +10,81 @@ public class CreditCard
 {
     private string cardNameValue;
     private byte[] cardImageValue;
+    private string cardLinkValue;
     private string cardTypeValue;
     private string cardEmploymentStatusValue;
     private string cardFeaturesValue;
     private string cardBalanceLengthValue;
     private string cardDischargedValue;
+    private string cardDateAddedValue;
+    private string cardTimeAddedValue;
+    private bool cardIsRemovedValue;
+    private string cardDateRemovedValue;
+    private string cardTimeRemovedValue;
 
     public string CardName
     {
         get { return cardNameValue; }
         set { cardNameValue = value; }
     }
-
     public byte[] CardImage
     {
         get { return cardImageValue; }
         set { cardImageValue = value; }
     }
-
+    public string CardLink
+    {
+        get { return cardLinkValue; }
+        set { cardLinkValue = value; }
+    }
     public string CardType
     {
         get { return cardTypeValue; }
         set { cardTypeValue = value; }
     }
-
     public string EmploymentStatus
     {
         get { return cardEmploymentStatusValue; }
         set { cardEmploymentStatusValue = value; }
     }
-
     public string Features
     {
         get { return cardFeaturesValue; }
         set { cardFeaturesValue = value; }
     }
-
     public string BalanceLength
     {
         get { return cardBalanceLengthValue; }
         set { cardBalanceLengthValue = value; }
     }
-
     public string Discharged
     {
         get { return cardDischargedValue; }
         set { cardDischargedValue = value; }
+    }
+    public string DateAdded
+    {
+        get { return cardDateAddedValue; }
+        set { cardDateAddedValue = value; }
+    }
+    public string TimeAdded
+    {
+        get { return cardTimeAddedValue; }
+        set { cardTimeAddedValue = value; }
+    }
+    public bool IsRemoved
+    {
+        get { return cardIsRemovedValue; }
+        set { cardIsRemovedValue = value; }
+    }
+    public string DateRemoved
+    {
+        get { return cardDateRemovedValue; }
+        set { cardTimeRemovedValue = value; }
+    }
+    public string TimeRemoved
+    {
+        get { return cardTimeRemovedValue; }
+        set { cardTimeRemovedValue = value; }
     }
 }
